1d96833a287a138e95366d6ee05fe72134d4bc59
roggc <roggc9@gmail.com>
2024-04-28 11:37:25 +0200

    clean project

--- src/components/range/index.spec.js
+++ src/components/range/index.spec.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen, fireEvent, cleanup } from "@testing-library/react";
 import Range from "./index.tsx";
 import { expect } from "chai";
-import { getElementClientCenter, sleep } from "../../helpers.js";
+import { getElementClientCenter, sleep, moveBullet } from "../../helpers.js";
 
 export default () =>
   describe("range", () => {
@@ -17,167 +17,48 @@ export default () =>
 
       const minBullet = screen.getByTestId("minBullet");
       const minBulletXBefore = minBullet.getBoundingClientRect().x;
-
       const maxBullet = screen.getByTestId("maxBullet");
-      // const maxBulletX = maxBullet.getBoundingClientRect().x;
+      const maxBulletXBefore = maxBullet.getBoundingClientRect().x;
 
-      // console.log("max...", maxBulletX);
       const { x: maxBulletCenterX } = getElementClientCenter(maxBullet);
 
-      // await waitFor(
-      //   () => {
-      //     expect(maxBulletX).to.be.greaterThan(100);
-      //   },
-      //   {
-      //     timeout: 1000,
-      //   }
-      // );
-
-      // console.log("maxBulletX", maxBulletX);
-
-      const portal = screen.getByTestId("portal");
-
-      const from = getElementClientCenter(minBullet);
-      const current = {
-        clientX: from.x,
-        clientY: from.y,
-      };
-      // fireEvent.mouseEnter(minBullet, current);
-      // fireEvent.mouseOver(minBullet, current);
-      fireEvent.mouseMove(minBullet, current);
-      fireEvent.mouseDown(minBullet, current);
-      // current.clientX += 100;
-      // current.clientY += 0;
-      // // await sleep(500);
-      // fireEvent.mouseMove(portal, current);
-      // // await sleep(500);
-      // fireEvent.mouseUp(minBullet, current);
-
-      // fireEvent.mouseDown(minBullet);
-      // fireEvent.mouseMove(minBullet, {
-      //   clientX: maxBullet.getBoundingClientRect().x,
-      // });
-      // fireEvent.mouseUp(minBullet);
-
-      // await user.pointer([
-      //   { target: minBullet },
-      //   "MouseLeft>",
-      //   { target: maxBullet },
-      //   "/MouseLeft",
-      // ]);
-
-      const { x: startX, y: startY } = getElementClientCenter(minBullet);
-      // const { x: endX, y: endY } = getElementClientCenter(maxBullet);
-      // console.log("endX", endX);
-
-      // await user.pointer([
-      //   {
-      //     target: minBullet,
-      //     keys: "MouseLeft>",
-      //     coords: { clientX: startX, clientY: startY },
-      //   },
-      //   { target: portal, coords: { clientX: startX + 10, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 20, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 30, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 40, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 50, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 60, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 70, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 80, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 90, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 100, clientY: startY } },
-      //   { target: portal, coords: { clientX: startX + 110, clientY: startY } },
-      //   {
-      //     target: minBullet,
-      //     keys: "/MouseLeft",
-      //     coords: { clientX: endX, clientY: startY },
-      //   },
-      // ]);
-
-      const longitude = maxBulletCenterX;
-      const numOfSteps = 60;
-      const steps = [...Array(numOfSteps).keys()];
-      let x;
-      for (const step of steps) {
-        x = startX + (step * longitude) / numOfSteps;
-        fireEvent.mouseMove(portal, { clientX: x, clientY: startY });
-        await new Promise((r) => setTimeout(r, 20));
-      }
-      const { x: finalX, y: finalY } = getElementClientCenter(minBullet);
-      fireEvent.mouseUp(minBullet, {
-        clientX: finalX,
-        clientY: finalY,
-      });
-      // fireEvent.mouseMove(portal, {
-      //   clientX: finalX - 1000,
-      //   clientY: finalY + 100,
-      // });
-      // await new Promise((r) => setTimeout(r, 10));
-
-      // await drag(minBullet, {
-      //   to: maxBullet,
-      //   // delta: { x: 999, y: 0 },
-      //   steps: 20,
-      //   duration: 1000,
-      // });
+      await moveBullet({ bullet: minBullet, longitude: maxBulletCenterX });
 
       const minBulletXAfter = minBullet.getBoundingClientRect().x;
-      // console.log("minBulletXAfter", minBulletXAfter);
+      const maxBulletXAfter = maxBullet.getBoundingClientRect().x;
 
       expect(minLabel).to.have.text("99€");
-
-      // const updatedLabel = await screen.findByText("99€");
-      // expect(updatedLabel).toBeInTheDocument();
-
-      // await waitFor(
-      //   () => {
-      //     expect(minLabel).to.have.text("99€");
-      //   },
-      //   {
-      //     timeout: 1000,
-      //   }
-      // );
-
       expect(minBulletXAfter).to.be.greaterThan(minBulletXBefore);
-      // done();
+      expect(maxBulletXAfter).to.be.equal(maxBulletXBefore);
     }).timeout(10000);
 
     it("move maxBullet to minBullet position", async () => {
       render(<Range min={1} max={100} />);
       await new Promise((r) => setTimeout(r, 10));
+
       const minLabel = screen.getByTestId("minLabel");
       expect(minLabel).to.have.text("1€");
       const maxLabel = screen.getByTestId("maxLabel");
       expect(maxLabel).to.have.text("100€");
+
       const minBullet = screen.getByTestId("minBullet");
-      // const { x: minBulletX } = getElementClientCenter(minBullet);
       const maxBullet = screen.getByTestId("maxBullet");
       const maxBulletXBefore = maxBullet.getBoundingClientRect().x;
-      const portal = screen.getByTestId("portal");
-      const from = getElementClientCenter(maxBullet);
-      const current = {
-        clientX: from.x,
-        clientY: from.y,
-      };
-      fireEvent.mouseMove(maxBullet, current);
-      fireEvent.mouseDown(maxBullet, current);
-      const { x: startX, y: startY } = getElementClientCenter(maxBullet);
-      const longitude = startX;
-      const numOfSteps = 60;
-      const steps = [...Array(numOfSteps).keys()];
-      for (const step of steps) {
-        const x = startX - (step * longitude) / numOfSteps;
-        fireEvent.mouseMove(portal, { clientX: x, clientY: startY });
-        await new Promise((r) => setTimeout(r, 10));
-      }
-      const { x: finalX, y: finalY } = getElementClientCenter(maxBullet);
-      fireEvent.mouseUp(maxBullet, {
-        clientX: finalX,
-        clientY: finalY,
+      const minBulletXBefore = minBullet.getBoundingClientRect().x;
+
+      const { x: startX } = getElementClientCenter(maxBullet);
+      await moveBullet({
+        bullet: maxBullet,
+        longitude: startX,
+        isMaxBullet: true,
       });
+
       const maxBulletXAfter = maxBullet.getBoundingClientRect().x;
+      const minBulletXAfter = minBullet.getBoundingClientRect().x;
+
       expect(maxLabel).to.have.text("2€");
       expect(maxBulletXAfter).to.be.lessThan(maxBulletXBefore);
+      expect(minBulletXAfter).to.be.equal(minBulletXBefore);
     }).timeout(10000);
 
     afterEach(cleanup);
--- src/helpers.js
+++ src/helpers.js
@@ -1,26 +1,4 @@
-import { fireEvent } from "@testing-library/react";
-
-function isElement(obj) {
-  if (typeof obj !== "object") {
-    return false;
-  }
-  let prototypeStr, prototype;
-  do {
-    prototype = Object.getPrototypeOf(obj);
-    // to work in iframe
-    prototypeStr = Object.prototype.toString.call(prototype);
-    // '[object Document]' is used to detect document
-    if (
-      prototypeStr === "[object Element]" ||
-      prototypeStr === "[object Document]"
-    ) {
-      return true;
-    }
-    obj = prototype;
-    // null is the terminal of object
-  } while (prototype !== null);
-  return false;
-}
+import { fireEvent, screen } from "@testing-library/react";
 
 export function getElementClientCenter(element) {
   const { left, top, width, height } = element.getBoundingClientRect();
@@ -29,40 +7,89 @@ export function getElementClientCenter(element) {
     y: top + height / 2,
   };
 }
-
-const getCoords = (charlie) =>
-  isElement(charlie) ? getElementClientCenter(charlie) : charlie;
-
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
-async function drag(element, { to: inTo, delta, steps = 20, duration = 500 }) {
-  const from = getElementClientCenter(element);
-  const to = delta
-    ? {
-        x: from.x + delta.x,
-        y: from.y + delta.y,
-      }
-    : getCoords(inTo);
-
-  const step = {
-    x: (to.x - from.x) / steps,
-    y: (to.y - from.y) / steps,
-  };
-
+export const moveBullet = async ({
+  bullet,
+  longitude,
+  isMaxBullet = false,
+  numOfSteps = 60,
+}) => {
+  const portal = screen.getByTestId("portal");
+  const from = getElementClientCenter(bullet);
   const current = {
     clientX: from.x,
     clientY: from.y,
   };
-
-  fireEvent.mouseEnter(element, current);
-  fireEvent.mouseOver(element, current);
-  fireEvent.mouseMove(element, current);
-  fireEvent.mouseDown(element, current);
-  for (let i = 0; i < steps; i++) {
-    current.clientX += step.x;
-    current.clientY += step.y;
-    await sleep(duration / steps);
-    fireEvent.mouseMove(element, current);
+  fireEvent.mouseMove(bullet, current);
+  fireEvent.mouseDown(bullet, current);
+  const steps = [...Array(numOfSteps).keys()];
+  for (const step of steps) {
+    const x = isMaxBullet
+      ? from.x - (step * longitude) / numOfSteps
+      : from.x + (step * longitude) / numOfSteps;
+    fireEvent.mouseMove(portal, { clientX: x, clientY: from.y });
+    await sleep(20);
   }
-  fireEvent.mouseUp(element, current);
-}
+  const { x: finalX, y: finalY } = getElementClientCenter(bullet);
+  fireEvent.mouseUp(bullet, {
+    clientX: finalX,
+    clientY: finalY,
+  });
+};
+
+// function isElement(obj) {
+//   if (typeof obj !== "object") {
+//     return false;
+//   }
+//   let prototypeStr, prototype;
+//   do {
+//     prototype = Object.getPrototypeOf(obj);
+//     // to work in iframe
+//     prototypeStr = Object.prototype.toString.call(prototype);
+//     // '[object Document]' is used to detect document
+//     if (
+//       prototypeStr === "[object Element]" ||
+//       prototypeStr === "[object Document]"
+//     ) {
+//       return true;
+//     }
+//     obj = prototype;
+//     // null is the terminal of object
+//   } while (prototype !== null);
+//   return false;
+// }
+
+// const getCoords = (charlie) =>
+//   isElement(charlie) ? getElementClientCenter(charlie) : charlie;
+
+// async function drag(element, { to: inTo, delta, steps = 20, duration = 500 }) {
+//   const from = getElementClientCenter(element);
+//   const to = delta
+//     ? {
+//         x: from.x + delta.x,
+//         y: from.y + delta.y,
+//       }
+//     : getCoords(inTo);
+
+//   const step = {
+//     x: (to.x - from.x) / steps,
+//     y: (to.y - from.y) / steps,
+//   };
+
+//   const current = {
+//     clientX: from.x,
+//     clientY: from.y,
+//   };
+
+//   fireEvent.mouseEnter(element, current);
+//   fireEvent.mouseOver(element, current);
+//   fireEvent.mouseMove(element, current);
+//   fireEvent.mouseDown(element, current);
+//   for (let i = 0; i < steps; i++) {
+//     current.clientX += step.x;
+//     current.clientY += step.y;
+//     await sleep(duration / steps);
+//     fireEvent.mouseMove(element, current);
+//   }
+//   fireEvent.mouseUp(element, current);
+// }
